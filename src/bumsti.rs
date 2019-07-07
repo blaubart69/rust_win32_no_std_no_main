b728a18beb9edf3f9aecc6740cc6924196095746
bee <bernhard.spindler.75@gmail.com>
2019-07-07 15:01:45 +0200

    working hello berni

--- src/bumsti.rs
+++ src/bumsti.rs
@@ -1,26 +1,28 @@
 use winapi::um::fileapi::WriteFile;
-use winapi::shared::minwindef::{UINT, DWORD, BOOL};
+use winapi::shared::minwindef::{DWORD, BOOL};
 use winapi::um::processenv::GetStdHandle;
 use winapi::um::winbase::STD_OUTPUT_HANDLE;
 use winapi::um::winnt::HANDLE;
 use winapi::um::minwinbase::LPOVERLAPPED;
+use winapi::ctypes::c_void;
+
 
 use core::str;
+use core::ptr::{null_mut, null};
 
 pub fn write_stdout(text: &str) -> BOOL {
+
     unsafe {
         let stdout: HANDLE = GetStdHandle(STD_OUTPUT_HANDLE);
 
-        let mut written : DWORD;
-        //let mut overlapped : LPOVERLAPPED;
-        let *mut overlapped : LPOVERLAPPED = core::ptr::null();
+        let mut written     : DWORD = 0;
 
         let ok = WriteFile(
             stdout,
-            "öjlöj",
-            3,
+            (text.as_ptr() as *const c_void),
+            text.len() as u32,
             &mut written,
-            mut overlapped
+            null_mut()
         );
 
         ok
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@
 use core::panic::PanicInfo;
 
 use winapi::um::processthreadsapi::ExitProcess;
-use winapi::um::fileapi::WriteFile;
 
 mod bumsti;
 
@@ -16,59 +15,8 @@ mod bumsti;
 #[no_mangle] // don't mangle the name of this function
 pub extern fn mainCRTStartup() -> ! {
 
-    let rc = bumsti::write_stdout("hello berni!");
+    let rc = bumsti::write_stdout("höllo berni!");
 
     unsafe { ExitProcess(128); }
     loop {}
 }
-
-// Pull in the system libc library for what crt0.o likely requires.
-//extern crate libc;
-
-/*
-#[start]
-fn start(_argc: isize, _argv: *const *const u8) -> isize {
-    0
-}
-*/
-/*
-#[no_mangle] // ensure that this symbol is called `main` in the output
-pub extern fn main(_argc: i32, _argv: *const *const u8) -> i32 {
-    0
-}
-*/
-/*
-#[entry]
-#[no_mangle]
-pub extern "system" fn mainCRTStartup()  {
-    unsafe
-        {
-            winapi::um::processthreadsapi::ExitProcess(99);
-        }
-}
-*/
-
-
-/*
-#[start]
-fn my_start(argc: isize, argv: *const *const u8) -> isize {
-    0
-}
-
-#[main]
-fn main() {
-
-}
-
-fn ixi() {
-    //bumsti::doTheExit(99);
-}
-*/
-
-
-/*
-#[panic_handler]
-fn my_panic(_info: &core::panic::PanicInfo) -> ! { loop {} }
-#[lang = "eh_personality"]
-extern "C" fn eh_personality() {}
-*/
