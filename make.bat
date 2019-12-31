75ead97ac7297ed729adb1108475f0e9b5ca400a
bee <bernhard.spindler.75@gmail.com>
2019-12-31 08:25:39 +0100

    first compiling version

--- make.bat
+++ make.bat
@@ -1,1 +1,3 @@
-cargo +nightly build
+@rem xargo +nightly build --target x86_64-pc-windows-msvc
+xargo +nightly build --release --target x86_64-pc-windows-msvc
+@rem xargo +nightly build -v  --target x86_64-pc-windows-none
